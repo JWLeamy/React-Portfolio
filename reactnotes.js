b63499dc0c7ca9242931cd56241ceec642103bef
John Leamy <jwleamy@gmail.com>
2023-04-12 17:23:16 -0700

    starter

--- reactnotes.js
+++ reactnotes.js
@@ -1,13 +1,17 @@
-// React can run on phones, tablets, tv's and even game console.
+/* React can run on phones, tablets, tv's and even game console.
 
-// react.dev is the react library library
-// react-dom is what connects to the app and maniltales the 
+ react.dev is the react library library
+ react-dom is what connects to the app and maniltales the 
 
-// _______SYNTAX FOR CRAEATING REACT ELEMENTS_______
 
-// createElement accepts 3 arguments that will establish the element you are trying to create
-// technically, this is an object
+createElement accepts 3 arguments that will establish the element you are trying to create
+technically, this is an object
+
+
+
+*/
 /*
+_______SYNTAX FOR CRAEATING REACT ELEMENTS_______
 1. Usually a string of the HTML element you want to make (in this case h1)
 2. Object cotaning any attribute or element you want to pass into the HTML element {if it has no att/elements, pass an empty object or null}
 3. The contents or children of the element you are creating (can be other ele)
