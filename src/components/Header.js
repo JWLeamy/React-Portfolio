0528558aab1e470a00c9584e17468b5a435f72f0
John Leamy <jwleamy@gmail.com>
2023-05-02 13:30:31 -0700

    reorganize

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,40 @@
 import React from "react";
 // Here we import the Header.css file to grant access to some additional classNameNames
 import "../styles/Header.css";
-import '../styles/Basic.css';
+import "../styles/Basic.css";
 
-const myname = ['J', 'o', 'h', 'n', ' ', 'L', 'e', 'a', 'm', 'y']
+
 // My name, opening title (what the client will see when they open the webpage)
 function Header() {
-  const nameEffect = myname.map((letter, index) => 
-    <span className ="nameletter l" style={`--i:${index}`}>{letter}</span>
-  )
-
-  console.log(nameEffect)
+  const name = "Welcome";
 
   return (
     <section className="one">
       <div className="homepage">
         <div className="title">
           <h1 className="opening l">
-            Welcome
+            {[...name].map((letter, index) => (
+              <span className="nameletter l" style={{ "--i": index + 1 }}>
+                {letter}
+              </span>
+            ))}
           </h1>
         </div>
         <h2 className="nameintro">
           <p>I'm John.</p>
         </h2>
         <div className="content">
-            <div className="containers">
-              <p className="text">
-                I develop
-              </p>          
-              <ul className="list">
-                <li className="item">organized</li>
-                <li className="item">efficient</li>
-                <li className="item">creative</li>
-                <li className="item">effective</li>
-              </ul>
-              <p className="closetext">
-                Full-Stack applications.
-              </p>
-            </div>
+          <div className="containers">
+            <p className="text">I develop</p>
+            <ul className="list">
+              <li className="item">organized</li>
+              <li className="item">efficient</li>
+              <li className="item">creative</li>
+              <li className="item">effective</li>
+            </ul>
+            <p className="closetext">Full-Stack applications.</p>
           </div>
+        </div>
       </div>
       <div className="appear">
         <div className="welcome">
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,66 +1,75 @@
-import React from 'react';
-// Here we import the Section.css file to grant access to some additional classNameNames
+import React, { useState } from 'react';
 import '../styles/Section.css';
 import '../styles/Basic.css';
-import Pixum from '../Images/FinalPixum.png'
-import Sneaky from '../Images/SneakyFinal.png'
-import GoB from '../Images/GoB.png'
+import Pixum from '../Images/FinalPixum.png';
+import Sneaky from '../Images/SneakyFinal.png';
+import GoB from '../Images/GoB.png';
 
 function Section() {
+  const [filters, setFilters] = useState([]);
+
+  const projects = [
+    {
+      name: 'Pixum',
+      img: Pixum,
+      url: 'https://mkalik.github.io/pixum/',
+      tags: ['full-stack', 'front-end'],
+      description: 'The easiest way to find new flix',
+    },
+    {
+      name: 'SneakyLink',
+      img: Sneaky,
+      url: 'https://sneakylink-project-2.herokuapp.com/',
+      tags: ['full-stack', 'back-end'],
+      description: 'Store all your socials in one place',
+    },
+    {
+      name: 'GoBommarito',
+      img: GoB,
+      url: 'https://go-bommarito.herokuapp.com/',
+      tags: ['full-stack', 'back-end'],
+      description: 'Blogs for travel, eating, and experience around the globe',
+    },
+  ];
+
+  const toggleFilter = (tag) => {
+    if (filters.includes(tag)) {
+      setFilters(filters.filter((f) => f !== tag));
+    } else {
+      setFilters([...filters, tag]);
+    }
+  };
+
+  const filteredProjects = filters.length > 0 ? projects.filter((project) => project.tags.some((tag) => filters.includes(tag))) : projects;
+
   return (
     <section className="three">
-                <div id="psections">
-                    <h2 className="projecthead">Projects and Applications</h2>
-                    <div className="filters">
-                        <ul>
-                            <li>Full-Stack</li>
-                            <li>Front-End</li>
-                            <li>Back-End</li>
-                            <li>HTML</li>
-                            <li>JavaScript</li>
-                            <li>NODE.js</li>
-                            <li>React.js</li>
-                            <li>MySQL</li>
-                            <li>MongoDB</li>
-                        </ul>
-                    </div>
-                    <div className="projects">
-                        <div className="cards">
-                            <h2 className='px'>Pixum</h2>
-                            <a href="https://mkalik.github.io/pixum/">
-                                <div className="blank-result-card">
-                                    <div className="app-container" >
-                                    <img src={Pixum} className="app-thumbnail" alt="project1" />
-                                    </div>
-                                </div>
-                            </a>
-                            <p className='appinfo'>The easiest way to find new flix</p>
-                        </div>
-                        <div className="cards">
-                            <h2 className='sl'>SneakyLink</h2>
-                            <a href="https://sneakylink-project-2.herokuapp.com/">
-                                <div className="blank-result-card">
-                                    <div className="app-container" >
-                                    <img src={Sneaky} className="app-thumbnail" alt="project2" />
-                                    </div>
-                                </div>
-                            </a>
-                            <p className='appinfo'>Store all your socials in one place</p>
-                        </div>
-                        <div className="cards">
-                            <h2 className='gob'>GoBommarito</h2>
-                            <a href="https://go-bommarito.herokuapp.com/">
-                                <div className="blank-result-card">
-                                    <div className="app-container" >
-                                    <img src={GoB} className="app-thumbnail" alt="project2" />
-                                    </div>
-                                </div>
-                            </a>
-                            <p className='appinfo'>Blogs for travel, eating, and experience around the globe</p>
-                        </div>
-                    </div>
+      <div id="psections">
+        <h2 className="projecthead">Projects and Applications</h2>
+        <div className="filters">
+          <ul>
+            <li onClick={() => toggleFilter('full-stack')} className={filters.includes('full-stack') ? 'active' : ''}>Full-Stack</li>
+            <li onClick={() => toggleFilter('front-end')} className={filters.includes('front-end') ? 'active' : ''}>Front-End</li>
+            <li onClick={() => toggleFilter('back-end')} className={filters.includes('back-end') ? 'active' : ''}>Back-End</li>
+          </ul>
+        </div>
+        <div className="projects">
+          {filteredProjects.map((project, index) => (
+            <div key={index} className={`cards ${!filters.length || project.tags.some((tag) => filters.includes(tag)) ? '' : 'hidden'}`}>
+              <h2 className={project.name}>{project.name}</h2>
+              <a href={project.url}>
+                <div className="blank-result-card">
+                  <div className="app-container">
+                    <img src={project.img} className="app-thumbnail" alt={project.name} />
+                  </div>
                 </div>
-            </section>
+              </a>
+              <p className="appinfo">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
