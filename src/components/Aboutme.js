29c835f5d2341a09f382f143672157e2782248fd
John Leamy <jwleamy@gmail.com>
2023-05-02 18:19:40 -0700

    color schemes

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -2,58 +2,62 @@ import React from "react";
 // Here we import the Navbar.css file to grant access to some additional classNameNameNames
 import "../styles/Aboutme.css";
 import "../styles/Basic.css";
-import hasbulla from "../Images/hasbullaclose.png";
+import me from "../Images/me.png";
 
 function Aboutme() {
   return (
     <section className="two" id="abmm">
       <div className="abmhead">
         <h2>About Me</h2>
-        <p>Full Stack Web Developer, Business Graduate, and Creator</p>
+        <p>Full Stack Web <span className="contrast">Developer,</span> Business <span className="contrast">Graduate</span>, and <span className="contrast">Creator</span></p>
       </div>
       <div className="profilebox">
         <div className="skillset">
 
         </div>
-        <div className="formats">
-          <div className="abm">
-            <div className="pic">
-              <img src={hasbulla} className="hasbulla" alt="hasbullaselfie" />
+        <div className="wordset">
+          <div className="formats">
+            <div className="abm">
+              <div className="pic">
+                <img src={me} className="hasbulla" alt="hasbullaselfie" />
+              </div>
+              <h2 className="picname">
+                <p>John</p>
+                <p>Leamy</p>
+              </h2>
+              <div className="pictitle">
+                <h2>Student</h2>
+              </div>
+              <ul className="piclist">
+              <li><a href="https://www.linkedin.com/in/john-leamy-b68676238/"><i className="fa-brands fa-linkedin fa-3x"></i></a></li>
+              <li ><a href="https://github.com/JWLeamy"><i className="fa-brands fa-github fa-3x"></i></a></li>
+              </ul>
             </div>
-            <h2 className="picname">
-              <p>John</p>
-              <p>Leamy</p>
-            </h2>
-            <div className="pictitle">
-              <h2>Student</h2>
-            </div>
-            <ul className="piclist">
-              <li>LinkedIn</li>
-              <li>Github</li>
-            </ul>
-          </div>
 
-          <div className="biotext">
-            <h2>W</h2>
-            <h3>I am and What I do</h3>
-            <div>
-              <button className="resume">Resume</button>
-              <button className="projectlink">Projects</button>
+            <div className="biotext">
+              <h2>Who</h2>
+              <h3>I am and What I do</h3>
+              <div>
+                <button className="resume">Resume</button>
+                <button className="projectlink">Projects</button>
+              </div>
+              <div className="bioinfo">
+                <p>
+                  Aspiring Develeoper with a drive to conceptualize, create, and
+                  ultimately provide useful full stack applications.
+                  <br />
+                  <br />
+                  Since writing my first line of code only four months ago, I've
+                  expanded my skillset across multiple programming languages and
+                  frameworks.
+                  <br />
+                  <br />
+                  This includes (but is not limited to) / JavaScript / Github /
+                  Node.js / Express.js / OOP / ORM / Heroku / MySQL / React & More{" "}
+                  <br />
+                </p>
+              </div>
             </div>
-            <p>
-              Aspiring Develeoper with a drive to conceptualize, create, and
-              ultimately provide useful full stack applications.
-              <br />
-              <br />
-              Since writing my first line of code only four months ago, I've
-              expanded my skillset across multiple programming languages and
-              frameworks.
-              <br />
-              <br />
-              This includes (but is not limited to) / JavaScript / Github /
-              Node.js / Express.js / OOP / ORM / Heroku / MySQL / React & More{" "}
-              <br />
-            </p>
           </div>
         </div>
       </div>
