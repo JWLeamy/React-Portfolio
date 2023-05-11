3fe24e2bca08b91621199e620a964008df5197da
John Leamy <jwleamy@gmail.com>
2023-05-11 12:48:36 -0700

    image hover effects

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import '../styles/Section.css';
-import '../styles/Basic.css';
-import Pixum from '../Images/FinalPixum.png';
-import Sneaky from '../Images/SneakyFinal.png';
-import GoB from '../Images/GoB.png';
+import React, { useState } from "react";
+import "../styles/Section.css";
+import "../styles/Basic.css";
+import Pixum from "../Images/FinalPixum.png";
+import Sneaky from "../Images/SneakyFinal.png";
+import GoB from "../Images/GoB.png";
 
 function Section() {
   const [filters, setFilters] = useState([]);
 
   const projects = [
     {
-      name: 'Pixum',
+      name: "Pixum",
       img: Pixum,
-      url: 'https://mkalik.github.io/pixum/',
-      tags: ['full-stack', 'front-end'],
-      description: 'The easiest way to find new flix',
+      url: "https://mkalik.github.io/pixum/",
+      tags: ["full-stack", "front-end"],
+      description: "The easiest way to find new flix",
     },
     {
-      name: 'SneakyLink',
+      name: "SneakyLink",
       img: Sneaky,
-      url: 'https://sneakylink-project-2.herokuapp.com/',
-      tags: ['full-stack', 'back-end'],
-      description: 'Store all your socials in one place',
+      url: "https://sneakylink-project-2.herokuapp.com/",
+      tags: ["full-stack", "back-end"],
+      description: "Store all your socials in one place",
     },
     {
-      name: 'GoBommarito',
+      name: "GoBommarito",
       img: GoB,
-      url: 'https://go-bommarito.herokuapp.com/',
-      tags: ['full-stack', 'back-end'],
-      description: 'Blogs for travel, eating, and experience around the globe',
+      url: "https://go-bommarito.herokuapp.com/",
+      tags: ["full-stack", "back-end"],
+      description: "Blogs for travel, eating, and experience around the globe",
     },
   ];
 
@@ -40,28 +40,31 @@ function Section() {
     }
   };
 
-  const filteredProjects = filters.length > 0 ? projects.filter((project) => project.tags.some((tag) => filters.includes(tag))) : projects;
+  const filteredProjects =
+    filters.length > 0
+      ? projects.filter((project) =>
+          project.tags.some((tag) => filters.includes(tag))
+        )
+      : projects;
 
   return (
     <section className="three" id="works">
-      <div className='fullthree'>
-        <div className='worksheader'>
+      <div className="fullthree">
+        <div className="worksheader">
           <h2 className="projecthead">Projects</h2>
           <p>A collection of works created in recent months</p>
           <p>This is where a few filters will be</p>
         </div>
         <div className="projects">
           {filteredProjects.map((project, index) => (
-            <div key={index} className={`cards ${!filters.length || project.tags.some((tag) => filters.includes(tag)) ? '' : 'hidden'}`}>
-              <h2 className={project.name}>{project.name}</h2>
+            
+            <div className="image" style={{ backgroundImage: `url(${project.img})` }}>
               <a href={project.url}>
-                <div className="blank-result-card">
-                  <div className="app-container">
-                    <img src={project.img} className="app-thumbnail" alt={project.name} />
-                  </div>
+                <div className="imageinfo">
+                  <h2 className={project.name}>{project.name}</h2>
+                  <p className="appinfo">{project.description}</p>
                 </div>
               </a>
-              <p className="appinfo">{project.description}</p>
             </div>
           ))}
         </div>
